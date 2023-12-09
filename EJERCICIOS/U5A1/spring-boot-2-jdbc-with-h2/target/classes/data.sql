19af30872a5b84d3cf992f439ad1c58c160640c6
Ignacio Bielsa <125258473+Kanutos@users.noreply.github.com>
2023-12-09 12:53:44 +0100

    Add files via upload

---
+++ EJERCICIOS/U5A1/spring-boot-2-jdbc-with-h2/target/classes/data.sql
@@ -0,0 +1,11 @@
+insert into estudiante
+values(101,'Lord voldermort', 'correo1@gmail.com');
+
+insert into estudiante
+values(102,'Marianicoelcorto Rajoy', 'correo2@gmail.com');
+
+insert into estudiante
+values(103,'Noviacadaver Ayuso', 'correo3@gmail.com');
+
+insert into estudiante
+values(104,'Pedro Sancheese', 'correo4@gmail.com');
